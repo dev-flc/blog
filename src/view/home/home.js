e1ddb7be5738c5915ee361fea1371932c879bf36
Fernando L.C <fer.lc.developer@gmail.com>
2022-09-22 22:58:54 -0500

    Initial commit

---
+++ src/view/home/home.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react"
+import { actionSetTitle } from "Actions/actionSetTitle.js"
+import { actionSetModeIsDark } from "Actions/actionsTheme.js"
+import { useStore } from "Hook/store/useStore.js"
+import { convertTextUpperOrLower } from "Utils/utils.js"
+
+import imageEslint from "Images/eslint.svg"
+import imagePrettier from "Images/prettier.svg"
+import imageReact from "Images/react.svg"
+import imageRedux from "Images/redux.svg"
+import imageSass from "Images/sass.svg"
+import imageWebpack from "Images/webpack.svg"
+
+import ButtonIcon from "Components/buttonIcon/buttonIcon.js"
+import DarkModeIcon from "@mui/icons-material/DarkMode"
+import LightModeIcon from "@mui/icons-material/LightMode"
+import TextRotateUpIcon from "@mui/icons-material/TextRotateUp"
+import TextRotateVerticalIcon from "@mui/icons-material/TextRotateVertical"
+
+import Container from "@mui/material/Container"
+
+const Home = React.memo(() => {
+  const [mapStateToProps, mapDispatchToProps] = useStore()
+  const [isUpperCase, setisUpperCase] = useState(false)
+
+  const { title, theme } = mapStateToProps
+
+  const { modeIsDark } = theme
+
+  const handleThemeMode = () => {
+    mapDispatchToProps(actionSetModeIsDark(!modeIsDark))
+  }
+
+  const handleConvertText = () => {
+    const { 0: text, 1: status } = convertTextUpperOrLower(title)
+    setisUpperCase(status)
+    mapDispatchToProps(actionSetTitle(text))
+  }
+
+  return (
+    <Container maxWidth="sm" fixed>
+      <div className="container">
+        <div className="sub-section">
+          <h1 className="title"> {title} </h1>
+        </div>
+        <div className="sub-section">
+          <img src={imageReact} className="image image-animation" alt="React" />
+          <img src={imageRedux} className="image image-animation" alt="Redux" />
+          <img src={imageSass} className="image" alt="Sass" />
+          <img src={imagePrettier} className="image" alt="Prettier" />
+          <img
+            src={imageEslint}
+            className="image image-animation"
+            alt="Eslint"
+          />
+          <img
+            src={imageWebpack}
+            className="image image-animation"
+            alt="Webpack"
+          />
+        </div>
+        <div>
+          <ButtonIcon
+            statusButton={isUpperCase}
+            onClick={handleConvertText}
+            iconOn={TextRotateUpIcon}
+            iconOff={TextRotateVerticalIcon}
+          />
+
+          <ButtonIcon
+            statusButton={modeIsDark}
+            onClick={handleThemeMode}
+            iconOn={LightModeIcon}
+            iconOff={DarkModeIcon}
+          />
+        </div>
+      </div>
+    </Container>
+  )
+})
+
+Home.displayName = "Home"
+
+export { Home }
